db2802de878e75d49b4161371682eba33a04ba86
nebelgrau <nebelgrau@gmx.com>
2020-07-19 11:25:08 +0200

    added more examples

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
 fn main () {
     
-    // example 1 
+    // examples
 
-    let mut arr1 = [2,5,3,7,1,4,5,6,7,6];
+    let mut arr = [2,5,3,7,1,4,5,6,7,6];
     
-    let sorted_arr1 = bubblesort(&mut arr1);
+    let sorted_arr = bubblesort(&mut arr);
 
-    for item in sorted_arr1.iter() {
+    println!("\nexample 1: integers");
+
+    for item in sorted_arr.iter() {
         println!("{}", item);
     }
 
-    // example 2
-
-    let mut arr2 = ["cat", "bat", "rat", "hat"];
+    let mut arr = [0.01, 0.002, 0.3, 0.011, 0.2, -0.01, 0.01];
     
-    let sorted_arr2 = bubblesort(&mut arr2);
+    let sorted_arr = bubblesort(&mut arr);
 
-    for item in sorted_arr2.iter() {
+    println!("\nexample 2: floats");
+
+    for item in sorted_arr.iter() {
+        println!("{}", item);
+    }
+    
+    let mut arr = ['x', 'a', 'b', 'x', 'p'];
+    
+    let sorted_arr = bubblesort(&mut arr);
+
+    println!("\nexample 3: chars");
+
+    for item in sorted_arr.iter() {
+        println!("{}", item);
+    }
+
+    let mut arr = ["cat", "bat", "rat", "hat"];
+    
+    let sorted_arr = bubblesort(&mut arr);
+
+    println!("\nexample 4: strings");
+
+    for item in sorted_arr.iter() {
         println!("{}", item);
     }
     
