6626476f70504220db3d0022404db504d360a98b
nebelgrau <nebelgrau@gmx.com>
2020-07-19 11:28:36 +0200

    added more examples

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,3 @@
-fn main () {
-    
-    // examples
-
-    let mut arr = [2,5,3,7,1,4,5,6,7,6];
-    
-    let sorted_arr = bubblesort(&mut arr);
-
-    println!("\nexample 1: integers");
-
-    for item in sorted_arr.iter() {
-        println!("{}", item);
-    }
-
-    let mut arr = [0.01, 0.002, 0.3, 0.011, 0.2, -0.01, 0.01];
-    
-    let sorted_arr = bubblesort(&mut arr);
-
-    println!("\nexample 2: floats");
-
-    for item in sorted_arr.iter() {
-        println!("{}", item);
-    }
-    
-    let mut arr = ['x', 'a', 'b', 'x', 'p'];
-    
-    let sorted_arr = bubblesort(&mut arr);
-
-    println!("\nexample 3: chars");
-
-    for item in sorted_arr.iter() {
-        println!("{}", item);
-    }
-
-    let mut arr = ["cat", "bat", "rat", "hat"];
-    
-    let sorted_arr = bubblesort(&mut arr);
-
-    println!("\nexample 4: strings");
-
-    for item in sorted_arr.iter() {
-        println!("{}", item);
-    }
-    
-}
-
-
 fn bubblesort<T: std::marker::Copy + std::cmp::PartialOrd>(arr: &mut [T]) -> &[T] {
     
     // bubble sort algorithm for generic types that implement Copy and PartialOrd
@@ -64,3 +17,54 @@ fn bubblesort<T: std::marker::Copy + std::cmp::PartialOrd>(arr: &mut [T]) -> &[T
     } 
     arr
 }
+
+
+fn main () {
+    
+    // examples
+
+    let mut arr = [2,5,3,7,1,4,5,6,7,6];
+    
+    let sorted_arr = bubblesort(&mut arr);
+
+    println!("\nexample 1: integers");
+
+    for item in sorted_arr.iter() {
+        print!("{}\t", item);
+    }
+
+    let mut arr = [0.01, 0.002, 0.3, 0.011, 0.2, -0.01, 0.01];
+    
+    let sorted_arr = bubblesort(&mut arr);
+
+    println!("\n\nexample 2: floats");
+
+    for item in sorted_arr.iter() {
+        print!("{}\t", item);
+    }
+    
+    let mut arr = ['x', 'a', 'b', 'x', 'p'];
+    
+    let sorted_arr = bubblesort(&mut arr);
+
+    println!("\n\nexample 3: chars");
+
+    for item in sorted_arr.iter() {
+        print!("{}\t", item);
+    }
+
+    let mut arr = ["cat", "bat", "rat", "hat"];
+    
+    let sorted_arr = bubblesort(&mut arr);
+
+    println!("\n\nexample 4: strings");
+
+    for item in sorted_arr.iter() {
+        print!("{}\t", item);
+    }
+    
+    print!("\n");
+
+}
+
+
